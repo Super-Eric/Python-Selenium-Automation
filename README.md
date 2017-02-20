233f284fa73b4da002b7c65b4f28876eebb51523
Super-Eric <dxlzxqxq@gmail.com>
2017-02-20 11:57:35 +0100

    Updated with the 2nd meeting minutes

--- README.md
+++ README.md
@@ -28,3 +28,23 @@
   * It would be recommended to install Firefox on Laptop, then use selenium with it. There is no need of other downloaded driver.
 
 ## 2. Tutorial
+### 1) Start Liclipse and create New Project
+* Click on “File”
+  * NEW >>> PyDev Project
+  * Add a new Project name (Example “TEST”)
+  * Grammar version 3.6
+  * Interpreter “Default”
+  * Click on “Finish”
+* Create new file:
+  * Right click on the Project name (“TEST”)
+  * Go to “NEW” >>> “File”, create a new file. (like “Test.py”)
+  * Click on “Finish”
+  * You created a new file and now you can add the script below to this file.
+  * Please be aware, that you have to update the “Chromedriver” part in the script.
+* Start The script:
+  * Now you can start the script:
+  * Right click on the project name
+  * Click on “Run as”
+  * Click on “1 Run Python”
+* Sample Code: [sample.py](https://github.com/Super-Eric/Python-Selenium-Automation/blob/master/sample.py)
+* Use Case 01: [uc01.py](https://github.com/Super-Eric/Python-Selenium-Automation/blob/master/uc01.py)
---
+++ sample.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chromedriver="C:\\Users\\c.lapenta\\Documents\\chromedriver_win32\\chromedriver.exe"
+os.environ["webdriver.chrome.driver"] = chromedriver
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(chromedriver)
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        ## Test Case
+        driver.get("http://www.rp-online.de/") #Navigate to URL
+        self.assertIn("RP ONLINE", driver.title) #Judge if string 1 in string 2
+        # Negative Case
+        # self.assertIn("Python", driver.title)
+        
+        #<input name="q" id="header-search-term" type="text" placeholder="Suchbegriff eingeben">
+        elem = driver.find_element_by_name("q") #Locate the search box
+        
+        elem.send_keys("Wetter") #Input search keyword
+        elem.send_keys(Keys.RETURN) # Click Enter button
+        assert "No results found." not in driver.page_source
+        # Negative Case
+        # assert "No results found." in driver.page_source
+        
+        #<div class="hover">
+        elems=driver.find_elements_by_class_name("hover")
+        print(elems[0].text)
+        #//*[@id="search-result-container"]/section/article[1]/a/header/div[2]
+
+    def tearDown(self):
+        self.driver.close()        
+
+if __name__ == "__main__":
+    unittest.main()
